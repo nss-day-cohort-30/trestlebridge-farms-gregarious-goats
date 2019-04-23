cd812d917dedbbe7998391373a8376e540d7880c
Prafullata Sonawane <prafullatass@gmail.com>
2019-04-23 13:48:49 -0500

    corrected flower names

--- src/Interfaces/INaturalFieldDwelling.cs
+++ src/Interfaces/INaturalFieldDwelling.cs
@@ -3,5 +3,6 @@ namespace Trestlebridge.Interfaces
     public interface INaturalFieldDwelling
     {
         // What do all natural field dwelling plants have to have?
+        string Type{get;}
     }
 }
