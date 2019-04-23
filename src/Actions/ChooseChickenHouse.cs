a09709cac1a84ca3b6ff2755bd91b5317644655e
Kirren Covey <kirrencovey@gmail.com>
2019-04-23 13:36:31 -0500

    redirects user to main menu if they have no animal fields/houses with vacancy

--- src/Actions/ChooseChickenHouse.cs
+++ src/Actions/ChooseChickenHouse.cs
@@ -5,30 +5,47 @@ using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
 using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Actions {
-    public class ChooseChickenHouse {
-        public static void CollectInput (Farm farm, IHouseDwelling animal) {
+namespace Trestlebridge.Actions
+{
+    public class ChooseChickenHouse
+    {
+        public static void CollectInput(Farm farm, IHouseDwelling animal) {
             //Console.Clear();
-            if (farm.ChickenHouses.Count() == 0) {
+            if (farm.ChickenHouses.Count() == 0)
+            {
                 Console.WriteLine("*** Oops! You need to purchase a chicken house first! ***");
                 Console.WriteLine("*** Press return key to go back to main menu.");
                 Console.ReadLine();
             } else {
+                int counter = 0;
                 for (int i = 0; i < farm.ChickenHouses.Count; i++)
                 {
-                    Console.WriteLine ($"{i + 1}. Chicken House ({farm.ChickenHouses[i]._animals.Count}/{farm.ChickenHouses[i].Capacity} animals)");
+                    // Only show facilities that are not full
+                    if (farm.GrazingFields[i]._animals.Count != farm.GrazingFields[i].Capacity)
+                    {
+                        Console.WriteLine($"{i + 1}. Chicken House ({farm.ChickenHouses[i]._animals.Count}/{farm.ChickenHouses[i].Capacity} animals)");
+                    } else {
+                        counter++;
+                    }
+
+                } // If all options are full, tell user to buy another one
+                if (counter == farm.GrazingFields.Count)
+                {
+                    Console.WriteLine("*** Oops! All your grazing fields are full, you need another one! ***");
+                    Console.WriteLine("*** Press return key to go back to main menu.");
+                    Console.ReadLine();
+                } else {
+                    Console.WriteLine();
+
+                    // How can I output the type of animal chosen here?
+                    Console.WriteLine($"Place the animal where?");
+
+                    Console.Write("> ");
+                    int choice = Int32.Parse(Console.ReadLine());
+
+                    farm.ChickenHouses[choice - 1].AddResource(farm, animal);
                 }
-
-                Console.WriteLine ();
-
-                // How can I output the type of animal chosen here?
-                Console.WriteLine ($"Place the animal where?");
-
-                Console.Write ("> ");
-                int choice = Int32.Parse(Console.ReadLine ());
-
-                farm.ChickenHouses[choice - 1].AddResource(farm, animal);
-
+            }
                 /*
                     Couldn't get this to work. Can you?
                     Stretch goal. Only if the app is fully functional.
@@ -37,4 +54,3 @@ namespace Trestlebridge.Actions {
             }
         }
     }
-}
