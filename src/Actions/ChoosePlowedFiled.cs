6407855aa2e6528ee49b97499b26efe8a86346ab
Prafullata Sonawane <prafullatass@gmail.com>
2019-04-23 10:47:16 -0500

    taking qty from user and making list of it

--- src/Actions/ChoosePlowedFiled.cs
+++ src/Actions/ChoosePlowedFiled.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
@@ -31,5 +32,37 @@ namespace Trestlebridge.Actions {
             // farm.PurchaseResource<IGrazing>(animal, choice);
 
         }
+        // public static void CollectInput (Farm farm, IPlowedFieldDwelling seeds) {
+        //     //Console.Clear();
+        //     List<Sesame> seeds = new List<Sesame>();
+        //     if(Int32.Parse(qty) != 1)
+        //     {
+        //         for(int i=0; i < Int32.Parse(qty); i++)
+        //             seeds.Add(new Sesame());
+        //     }
+
+
+        //     for (int i = 0; i < farm.PlowedFields.Count; i++)
+        //     {
+        //         Console.WriteLine ($"{i + 1}. Plowed Field");
+        //     }
+
+        //     Console.WriteLine ();
+
+        //     // How can I output the type of animal chosen here?
+        //     Console.WriteLine ($"Where would you like to plant the seeds?");
+
+        //     Console.Write ("> ");
+        //     int choice = Int32.Parse(Console.ReadLine ());
+
+        //     farm.PlowedFields[choice-1].AddResource(seeds);
+
+        //     /*
+        //         Couldn't get this to work. Can you?
+        //         Stretch goal. Only if the app is fully functional.
+        //      */
+        //     // farm.PurchaseResource<IGrazing>(animal, choice);
+
+      //  }
     }
 }
--- src/Actions/PurchaseSeeds.cs
+++ src/Actions/PurchaseSeeds.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
@@ -18,13 +19,48 @@ namespace Trestlebridge.Actions {
             Console.Write ("> ");
             string choice = Console.ReadLine ();
 
+            Console.WriteLine ();
+            Console.WriteLine ("How many Sunflower would you like to plant?");
+
+            Console.Write ("> ");
+            string qty = Console.ReadLine ();
+            List<IPlowedFieldDwelling> SesameSeeds = new List<IPlowedFieldDwelling>();
+            List<INaturalFieldDwelling> WildFlowerSeeds = new List<INaturalFieldDwelling>();
+            if (Int32.Parse(choice) == 1 )
+            {
+                if(Int32.Parse(qty) != 1)
+                {
+                    for(int i=0; i < Int32.Parse(qty); i++)
+                        SesameSeeds.Add(new Sesame());
+                }
+            }
+            else
+            {
+                if (Int32.Parse(choice) == 2 )
+                {
+                    if(Int32.Parse(qty) != 1)
+                    {
+                        for(int i=0; i < Int32.Parse(qty); i++)
+                            WildFlowerSeeds.Add(new WildFlower());
+                    }
+                }
+            }
+
+
             switch (Int32.Parse(choice))
             {
                 case 1:
-                    ChoosePlowedField.CollectInput(farm, new Sesame());
+                    if(Int32.Parse(qty) == 1)
+                        ChoosePlowedField.CollectInput(farm, new Sesame());
+                    else
+                        ChoosePlowedField.CollectInput(farm, SesameSeeds);
                     break;
                 case 2:
-                    ChooseNaturalField.CollectInput(farm, new WildFlower());
+                    if(Int32.Parse(qty) == 1)
+                        ChooseNaturalField.CollectInput(farm, new WildFlower());
+                    else
+                        ChooseNaturalField.CollectInput(farm, WildFlowerSeeds);
+
                     break;
                 case 3:
                     ChooseBothField.CollectInput(farm, new SunFlower());
