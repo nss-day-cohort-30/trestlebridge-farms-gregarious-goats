d810e8b84ea48d9b47bd1b18676983fc0e67a885
Prafullata Sonawane <prafullatass@gmail.com>
2019-04-23 10:59:14 -0500

    added overloading funtion in chooseNaturalField

--- src/Actions/ChooseNaturalField.cs
+++ src/Actions/ChooseNaturalField.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
@@ -7,6 +8,35 @@ using Trestlebridge.Models.Animals;
 namespace Trestlebridge.Actions {
     public class ChooseNaturalField {
         public static void CollectInput (Farm farm, INaturalFieldDwelling seed) {
+            //Console.Clear();
+            if (farm.NaturalFields.Count() == 0) {
+                Console.WriteLine("*** Oops! You need to purchase a natural field first! ***");
+                Console.WriteLine("*** Press return key to go back to main menu.");
+                Console.ReadLine();
+            } else {
+                for (int i = 0; i < farm.NaturalFields.Count; i++)
+                {
+                    Console.WriteLine ($"{i + 1}. Natural Field ({farm.NaturalFields[i]._plants.Count} rows of plants)");
+                }
+
+                Console.WriteLine ();
+
+                // How can I output the type of animal chosen here?
+                Console.WriteLine ($"Where would you like to plant the Sunflowers?");
+
+                Console.Write ("> ");
+                int choice = Int32.Parse(Console.ReadLine ());
+
+                farm.NaturalFields[choice-1].AddResource(farm, seed);
+
+                /*
+                    Couldn't get this to work. Can you?
+                    Stretch goal. Only if the app is fully functional.
+                */
+                // farm.PurchaseResource<IGrazing>(animal, choice);
+            }
+        }
+        public static void CollectInput (Farm farm, List<INaturalFieldDwelling> seed) {
             //Console.Clear();
             if (farm.NaturalFields.Count() == 0) {
                 Console.WriteLine("*** Oops! You need to purchase a natural field first! ***");
--- src/Actions/ChoosePlowedFiled.cs
+++ src/Actions/ChoosePlowedFiled.cs
@@ -37,37 +37,31 @@ namespace Trestlebridge.Actions {
                 // farm.PurchaseResource<IGrazing>(animal, choice);
             }
         }
-        // public static void CollectInput (Farm farm, IPlowedFieldDwelling seeds) {
-        //     //Console.Clear();
-        //     List<Sesame> seeds = new List<Sesame>();
-        //     if(Int32.Parse(qty) != 1)
-        //     {
-        //         for(int i=0; i < Int32.Parse(qty); i++)
-        //             seeds.Add(new Sesame());
-        //     }
 
+        public static void CollectInput(Farm farm, List<IPlowedFieldDwelling> sesameSeeds)
+        {
 
-        //     for (int i = 0; i < farm.PlowedFields.Count; i++)
-        //     {
-        //         Console.WriteLine ($"{i + 1}. Plowed Field");
-        //     }
+            for (int i = 0; i < farm.PlowedFields.Count; i++)
+            {
+                Console.WriteLine ($"{i + 1}. Plowed Field");
+            }
 
-        //     Console.WriteLine ();
+            Console.WriteLine ();
 
-        //     // How can I output the type of animal chosen here?
-        //     Console.WriteLine ($"Where would you like to plant the seeds?");
+            // How can I output the type of animal chosen here?
+            Console.WriteLine ($"Where would you like to plant the seeds?");
 
-        //     Console.Write ("> ");
-        //     int choice = Int32.Parse(Console.ReadLine ());
+            Console.Write ("> ");
+            int choice = Int32.Parse(Console.ReadLine ());
 
-        //     farm.PlowedFields[choice-1].AddResource(seeds);
+            farm.PlowedFields[choice-1].AddResource(sesameSeeds);
 
-        //     /*
-        //         Couldn't get this to work. Can you?
-        //         Stretch goal. Only if the app is fully functional.
-        //      */
-        //     // farm.PurchaseResource<IGrazing>(animal, choice);
+            /*
+                Couldn't get this to work. Can you?
+                Stretch goal. Only if the app is fully functional.
+             */
+            // farm.PurchaseResource<IGrazing>(animal, choice);
 
-      //  }
+         }
     }
 }
