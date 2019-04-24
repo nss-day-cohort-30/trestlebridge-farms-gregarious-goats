a0843535433ad45a0731a19755ed0e6440cccffd
Connor Bailey <bailey.connor.p@gmail.com>
2019-04-24 11:47:05 -0500

    refactored purchaseSeeds to use a dynamic plant class. Instead of making different instances by the type of plant, we can now make instances based off of the plants' interfaces

--- src/Actions/PurchaseSeeds.cs
+++ src/Actions/PurchaseSeeds.cs
@@ -2,87 +2,91 @@ using System;
 using System.Collections.Generic;
 using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
+using Trestlebridge.Models.BaseClasses;
 using Trestlebridge.Models.Animals;
 using Trestlebridge.Models.Facilities;
 using Trestlebridge.Models.Plants;
 
-namespace Trestlebridge.Actions {
-    public class PurchaseSeeds {
-        public static void CollectInput (Farm farm) {
-            Console.WriteLine ("1. Sesame");
-            Console.WriteLine ("2. Wild Flower");
-            Console.WriteLine ("3. Sunflower");
-
-            Console.WriteLine ();
-            Console.WriteLine ("What are you buying today?");
-
-            Console.Write ("> ");
-            string choice = Console.ReadLine ();
-
-            Console.WriteLine ();
-            Console.WriteLine ("How many would you like to plant?");
-
-            Console.Write ("> ");
-            string qty = Console.ReadLine ();
-            List<IPlowedFieldDwelling> SesameSeeds = new List<IPlowedFieldDwelling>();
-            List<INaturalFieldDwelling> WildFlowerSeeds = new List<INaturalFieldDwelling>();
-            List<SunFlower> sunflowerSeeds = new List<SunFlower>();
-            if (Int32.Parse(choice) == 1 )
+namespace Trestlebridge.Actions
+{
+    public class PurchaseSeeds
+    {
+        // This function returns a list of plants of a specific interface, based on which interfaces the users chosen plant type has (used for if the user wants to buy more than 1 plant simultaneously)
+        public static List<T> MakeListOfPlants<T>(int qty, T plant)
+        {
+            List<T> ListOfChosenPlants = new List<T>();
+            for (int i = 0; i < qty; i++)
             {
-                if(Int32.Parse(qty) != 1)
+                ListOfChosenPlants.Add(plant);
+            }
+            return ListOfChosenPlants;
+        }
+
+        public static void CollectInput(Farm farm)
+        {
+            // make a list of available plant classes
+            List<Plant> AvailablePlants = new List<Plant>
+            {
+                new Sesame(), new WildFlower(), new SunFlower()
+            };
+            // make a menu that mirrors the list AvailablePlants
+            Console.WriteLine("1. Sesame");
+            Console.WriteLine("2. Wild Flower");
+            Console.WriteLine("3. Sunflower");
+
+
+            Console.WriteLine();
+            Console.WriteLine("What are you buying today?");
+
+            Console.Write("> ");
+            int choice = Int32.Parse(Console.ReadLine());
+
+            // make a new object of whatever type of plant the user chose
+            dynamic chosenPlantType = Activator.CreateInstance(AvailablePlants[choice - 1].GetType());
+
+            Console.WriteLine();
+            Console.WriteLine("How many would you like to plant?");
+
+            Console.Write("> ");
+            int qty = Int32.Parse(Console.ReadLine());
+
+
+            // Checks to see if the plant can be planted in BOTH Plowed AND Natural fields
+            if (chosenPlantType is IAnyFieldDwelling)
+            {
+                if (qty != 1)
                 {
-                    for(int i=0; i < Int32.Parse(qty); i++)
-                        SesameSeeds.Add(new Sesame());
+                    List<IAnyFieldDwelling> plants = MakeListOfPlants(qty, (IAnyFieldDwelling)chosenPlantType);
+                    ChooseBothField.CollectInput(farm, plants);
+                }
+                else
+                {
+                    ChooseBothField.CollectInput(farm, (IAnyFieldDwelling)chosenPlantType);
+                }
+            }
+            else if (chosenPlantType is IPlowedFieldDwelling)
+            {
+                if (qty != 1)
+                {
+                    List<IPlowedFieldDwelling> plants = MakeListOfPlants(qty, (IPlowedFieldDwelling)chosenPlantType);
+                    ChoosePlowedField.CollectInput(farm, plants);
+                }
+                else
+                {
+                    ChoosePlowedField.CollectInput(farm, (IPlowedFieldDwelling)chosenPlantType);
                 }
             }
             else
             {
-                if (Int32.Parse(choice) == 2 )
+                if (qty != 1)
                 {
-                    if(Int32.Parse(qty) != 1)
-                    {
-                        for(int i=0; i < Int32.Parse(qty); i++)
-                            WildFlowerSeeds.Add(new WildFlower());
-                    }
+                    List<INaturalFieldDwelling> plants = MakeListOfPlants(qty, (INaturalFieldDwelling)chosenPlantType);
+                    ChooseNaturalField.CollectInput(farm, plants);
                 }
                 else
                 {
-                    if (Int32.Parse(choice) == 3)
-                    {
-                        if(Int32.Parse(qty) != 1)
-                        {
-                            for(int i=0; i < Int32.Parse(qty); i++)
-                                sunflowerSeeds.Add(new SunFlower());
-                        }
-                    }
+                    ChooseNaturalField.CollectInput(farm, (INaturalFieldDwelling)chosenPlantType);
                 }
-            }
-
-
-            switch (Int32.Parse(choice))
-            {
-                case 1:
-                    if(Int32.Parse(qty) == 1)
-                        ChoosePlowedField.CollectInput(farm, new Sesame());
-                    else
-                        ChoosePlowedField.CollectInput(farm, SesameSeeds);
-                    break;
-                case 2:
-                    if(Int32.Parse(qty) == 1)
-                        ChooseNaturalField.CollectInput(farm, new WildFlower());
-                    else
-                        ChooseNaturalField.CollectInput(farm, WildFlowerSeeds);
-
-                    break;
-                case 3:
-                    if(Int32.Parse(qty) == 1)
-
-                        ChooseBothField.CollectInput(farm, new SunFlower());
-                    else
-                        ChooseBothField.CollectInput(farm, sunflowerSeeds);
-                    break;
-                default:
-                    break;
             }
         }
     }
