fa7e4eedec136f0f1befef1e755b135ee86a2ad8
Connor Bailey <bailey.connor.p@gmail.com>
2019-04-26 14:12:40 -0500

    farm is now capable of compressing animals into eggs...

--- src/Actions/ChooseEggGatherer.cs
+++ src/Actions/ChooseEggGatherer.cs
@@ -57,54 +57,125 @@ namespace Trestlebridge.Actions
                 }
 
 
-
-
                 Console.WriteLine();
                 Console.WriteLine($"Which facility has the animals you want to process?");
                 Console.Write("> ");
 
-                int choice = Int32.Parse(Console.ReadLine());
+                int facilityChoiceNum = Int32.Parse(Console.ReadLine()) - 1;
+                if (facilityChoiceNum <= FacilitiesThatHaveEggAnimals.Count)
+                {
+                    var chosenFacility = FacilitiesThatHaveEggAnimals[facilityChoiceNum];
+                    var chosenFacilityId = chosenFacility.ShortId;
+                    var chosenFacilityAnimalTypes = chosenFacility.Resources
+                    .GroupBy(animal => animal.Type)
+                    .Select(grp => grp.ToList())
+                    .ToList();
+                    Console.WriteLine("The following resource types are in that facility:");
+                    for (int i = 0; i < chosenFacilityAnimalTypes.Count; i++)
+                    {
+                        Console.WriteLine($"{i + 1}: {chosenFacilityAnimalTypes[i].Count} x {chosenFacilityAnimalTypes[i][0].Type}");
+                    }
 
-                //         if (choice <= farm.GrazingFields.Count)
-                //         {
-                //             Console.WriteLine("The following animals are in the grazing field:");
-                //             var groupedAnimals = farm.GrazingFields[choice - 1].Resources.GroupBy(
-                //                     currentAnimal => currentAnimal.Type
-                //                     );
-                //             var i = 1;
-                //             foreach (var currentAnimalGroup in groupedAnimals)
-                //             {
-                //                 Console.WriteLine($"{i}. {currentAnimalGroup.Count() + " " + currentAnimalGroup.Key}");
-                //                 i++;
-                //             };
+                    Console.WriteLine();
+                    Console.WriteLine("Which resource would you like to COMPRESS INTO EGGS?");
+                    Console.Write("> ");
 
-                //             Console.WriteLine();
-                //             Console.WriteLine("Which resource should be processed?");
-                //             Console.Write("> ");
 
-                //             int input = Int32.Parse(Console.ReadLine());
+                    int animalTypeChoiceNum = Int32.Parse(Console.ReadLine()) - 1;
 
-                //         }
-                //         else
-                //         {
-                //             Console.WriteLine("The following animals are in the chicken house:");
-                //             var groupedAnimals = farm.ChickenHouses[choice - farm.GrazingFields.Count - 1].Resources.GroupBy(
-                //                     currentAnimal => currentAnimal.Type
-                //                     );
-                //             var i = 1;
-                //             foreach (var currentAnimalGroup in groupedAnimals)
-                //             {
-                //                 Console.WriteLine($"{i}. {currentAnimalGroup.Count() + " " + currentAnimalGroup.Key}");
-                //                 i++;
-                //             };
-                //             Console.WriteLine();
-                //             Console.WriteLine("Which resource should be processed?");
-                //             Console.Write("> ");
+                    string chosenAnimalType = chosenFacilityAnimalTypes[animalTypeChoiceNum][0].Type;
+                    dynamic resourceClassTemplate = Activator.CreateInstance(chosenFacilityAnimalTypes[animalTypeChoiceNum][0].GetType());
+                    double eggConversionMultiplier = ((IEggProducing)chosenFacilityAnimalTypes[animalTypeChoiceNum][0])._eggsProduced;
 
-                //             int input = Int32.Parse(Console.ReadLine());
-                //         }
-                //     }
-                //     catch (FormatException) { }
+                    if (animalTypeChoiceNum <= chosenFacilityAnimalTypes.Count)
+                    {
+                        Console.WriteLine($"How many of this type of resource would you like to COMPRESS INTO EGGS?");
+                        Console.Write("> ");
+
+                        int numResourcesToProcessNum = Int32.Parse(Console.ReadLine());
+
+                        if (numResourcesToProcessNum <= chosenFacilityAnimalTypes[animalTypeChoiceNum].Count)
+                        {
+                            int EggsReturned = numResourcesToProcessNum * Convert.ToInt32(eggConversionMultiplier);
+
+                            Console.WriteLine($"Are you sure you want to process these into {EggsReturned} eggs? (y or n)");
+                            Console.WriteLine("> ");
+
+                            string yesOrNo = Console.ReadLine();
+                            if (yesOrNo == "y")
+                            {
+                                Console.WriteLine($"You did what you had to do. You have compressed {numResourcesToProcessNum} {chosenAnimalType} into {EggsReturned} eggs...");
+
+                                try
+                                {
+                                    for (var i = 0; i <= numResourcesToProcessNum - 1; i++)
+                                        farm.GrazingFields.Single(field => field.ShortId == chosenFacilityId).Resources
+                                        .RemoveAt(farm.GrazingFields.Single(field => field.ShortId == chosenFacilityId).Resources.Count - 1);
+                                }
+                                catch
+                                {
+                                }
+                                try
+                                {
+                                    for (var i = 0; i <= numResourcesToProcessNum - 1; i++)
+                                        farm.PlowedFields.Single(field => field.ShortId == chosenFacilityId).Resources
+                                        .RemoveAt(farm.PlowedFields.Single(field => field.ShortId == chosenFacilityId).Resources.Count - 1);
+                                }
+                                catch
+                                {
+
+                                }
+                                try
+                                {
+                                    for (var i = 0; i <= numResourcesToProcessNum - 1; i++)
+                                        farm.NaturalFields.Single(field => field.ShortId == chosenFacilityId).Resources
+                                        .RemoveAt(farm.NaturalFields.Single(field => field.ShortId == chosenFacilityId).Resources.Count - 1);
+                                }
+                                catch
+                                {
+
+                                }
+                                try
+                                {
+                                    for (var i = 0; i <= numResourcesToProcessNum - 1; i++)
+                                        farm.ChickenHouses.Single(field => field.ShortId == chosenFacilityId).Resources
+                                        .RemoveAt(farm.ChickenHouses.Single(field => field.ShortId == chosenFacilityId).Resources.Count - 1);
+                                }
+                                catch
+                                {
+
+                                }
+                                try
+                                {
+                                    for (var i = 0; i <= numResourcesToProcessNum - 1; i++)
+                                        farm.DuckHouses.Single(field => field.ShortId == chosenFacilityId).Resources
+                                        .RemoveAt(farm.DuckHouses.Single(field => field.ShortId == chosenFacilityId).Resources.Count - 1);
+                                }
+                                catch
+                                {
+
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("You are truly merciful...");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("You dont have enough of that resource to compress into that many eggs....");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid choice, rerouting to main menu");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice, rerouting to main menu");
+                }
+
             }
         }
     }
